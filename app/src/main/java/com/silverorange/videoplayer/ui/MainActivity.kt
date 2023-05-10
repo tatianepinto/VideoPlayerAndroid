2658dd04858cfd5c477cb73d911b141edca59b92
tatianepinto <tatiane.pinto.dev@outlook.com>
2023-05-10 16:56:29 -0400

    Showing the status from view model to main activity

--- app/src/main/java/com/silverorange/videoplayer/ui/MainActivity.kt
+++ app/src/main/java/com/silverorange/videoplayer/ui/MainActivity.kt
@@ -2,11 +2,19 @@ package com.silverorange.videoplayer.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.silverorange.videoplayer.R
+import com.silverorange.videoplayer.overview.ViewModel
+import kotlinx.android.synthetic.main.activity_main.et_name
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: ViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(ViewModel::class.java)
+
+        et_name.text = viewModel.status.toString()
     }
 }
