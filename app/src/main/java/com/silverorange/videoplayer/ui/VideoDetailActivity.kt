c701029e42c1311c4c340389accd5692b3c04421
tatianepinto <tatiane.pinto.dev@outlook.com>
2023-05-11 15:56:02 -0400

    Add back button in the menu

--- app/src/main/java/com/silverorange/videoplayer/ui/VideoDetailActivity.kt
+++ app/src/main/java/com/silverorange/videoplayer/ui/VideoDetailActivity.kt
@@ -2,6 +2,8 @@ package com.silverorange.videoplayer.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.silverorange.videoplayer.R
 
 class VideoDetailActivity : AppCompatActivity() {
@@ -9,4 +11,21 @@ class VideoDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_detail)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_detail, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_back -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 }
