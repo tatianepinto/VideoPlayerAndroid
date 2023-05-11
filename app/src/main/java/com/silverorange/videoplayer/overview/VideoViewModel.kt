bfd306ffb14f50d1a2e61edb1b578ea9f1428f1e
tatianepinto <tatiane.pinto.dev@outlook.com>
2023-05-11 12:20:51 -0400

    Created val for retrieving video data

--- app/src/main/java/com/silverorange/videoplayer/overview/VideoViewModel.kt
+++ app/src/main/java/com/silverorange/videoplayer/overview/VideoViewModel.kt
@@ -6,19 +6,23 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.silverorange.videoplayer.network.Api
 import androidx.lifecycle.viewModelScope
+import com.silverorange.videoplayer.network.Video
 import kotlinx.coroutines.launch
 
 class VideoViewModel: ViewModel() {
 
     private val _status = MutableLiveData<String>()
+    private val _allVideos = MutableLiveData<List<Video>>()
 
     val status: LiveData<String> = _status
+    val allVideos: LiveData<List<Video>> = _allVideos
 
     private fun getAllVideos() {
         viewModelScope.launch {
             try {
                 val listResult = Api.retrofitService.getVideos()
                 _status.value = "Success: ${listResult.size} videos retrieved"
+                _allVideos.value = listResult
             } catch (e: Exception) {
                 e.message?.let { Log.d("VideoPlayerCheck", it) }
                 _status.value = "Failure: ${e.message}"
